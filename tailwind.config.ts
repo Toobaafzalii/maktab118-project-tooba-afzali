738cd34d37285378b716ba46c67c6f4c17a89f62
Tooba Afzali <toobaafzalii78@gmail.com>
2024-11-28 14:06:48 +0330

    feat: add costum fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,17 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        "app-display": ["app-display-font", "sans-serif"],
+        "app-peyda": ["app-peyda-black", "sans-serif"],
+        "app-peyda-bold": ["app-peyda-bold", "sans-serif"],
+        "app-peyda-extrabold": ["app-peyda-extrabold", "sans-serif"],
+        "app-peyda-extralight": ["app-peyda-extralight", "sans-serif"],
+        "app-peyda-light": ["app-peyda-light", "sans-serif"],
+        "app-peyda-medium": ["app-peyda-medium", "sans-serif"],
+        "app-peyda-regular": ["app-peyda-regular", "sans-serif"],
+        "app-peyda-semibold": ["app-peyda-semibold", "sans-serif"],
+        "app-peyda-thin": ["app-peyda-thin", "sans-serif"],
       },
     },
   },
